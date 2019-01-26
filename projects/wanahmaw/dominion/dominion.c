07a36905a5b58c01dc5c9afe1d7d810d83eb533a
WanAshraf WanAhmadEzani <wanahmaw@flip2.engr.oregonstate.edu>
2019-01-25 23:19:01 -0800

    Moved implementation of Minion into a separate function outside of switch statement

--- projects/wanahmaw/dominion/dominion.c
+++ projects/wanahmaw/dominion/dominion.c
@@ -683,6 +683,61 @@ void run_Smithy(struct gameState *state, int handPos, int currentPlayer)
   discardCard(handPos, currentPlayer, state, 0);
 }
 
+void run_Minion(struct gameState *state, int handPos, int currentPlayer, int choice1, int choice2)
+{
+  int i;
+  int j;
+  
+  //+1 action
+  state->numActions++;
+		    
+  //discard card from hand
+  discardCard(handPos, currentPlayer, state, 0);
+		    
+  if (choice1)		//+2 coins
+    {
+      state->coins = state->coins + 2;
+    }
+		    
+  else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+    {
+      //discard hand
+      while(numHandCards(state) > 0)
+	{
+	  discardCard(handPos, currentPlayer, state, 0);
+	}
+			    
+      //draw 4
+      for (i = 0; i < 4; i++)
+	{
+	  drawCard(currentPlayer, state);
+	}
+			    
+      //other players discard hand and redraw if hand size > 4
+      for (i = 0; i < state->numPlayers; i++)
+	{
+	  if (i != currentPlayer)
+	    {
+	      if ( state->handCount[i] > 4 )
+		{
+		  //discard hand
+		  while( state->handCount[i] > 0 )
+		    {
+		      discardCard(handPos, i, state, 0);
+		    }
+						    
+		  //draw 4
+		  for (j = 0; j < 4; j++)
+		    {
+		      drawCard(i, state);
+		    }
+		}
+	    }
+	}
+			    
+    }
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -925,54 +980,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case minion:
-      //+1 action
-      state->numActions++;
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-			
-      if (choice1)		//+2 coins
-	{
-	  state->coins = state->coins + 2;
-	}
-			
-      else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
-	{
-	  //discard hand
-	  while(numHandCards(state) > 0)
-	    {
-	      discardCard(handPos, currentPlayer, state, 0);
-	    }
-				
-	  //draw 4
-	  for (i = 0; i < 4; i++)
-	    {
-	      drawCard(currentPlayer, state);
-	    }
-				
-	  //other players discard hand and redraw if hand size > 4
-	  for (i = 0; i < state->numPlayers; i++)
-	    {
-	      if (i != currentPlayer)
-		{
-		  if ( state->handCount[i] > 4 )
-		    {
-		      //discard hand
-		      while( state->handCount[i] > 0 )
-			{
-			  discardCard(handPos, i, state, 0);
-			}
-							
-		      //draw 4
-		      for (j = 0; j < 4; j++)
-			{
-			  drawCard(i, state);
-			}
-		    }
-		}
-	    }
-				
-	}
+      run_Minion(state, handPos, currentPlayer, choice1, choice2);
       return 0;
 		
     case steward:
