69b2202cd7752ce689f622eed9180e18e1647773
WanAshraf WanAhmadEzani <wanahmaw@flip2.engr.oregonstate.edu>
2019-01-26 00:04:34 -0800

    Moved implementation of Remodel into a function

--- projects/wanahmaw/dominion/dominion.c
+++ projects/wanahmaw/dominion/dominion.c
@@ -792,6 +792,7 @@ int run_Remodel(struct gameState *state, int currentPlayer, int choice1, int cho
     }
 
   return 0;
+  // testing
 }
 
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
