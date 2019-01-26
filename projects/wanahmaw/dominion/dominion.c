d4427e11a89ef2abd9f641c1d6525200a1f0a3b7
WanAshraf WanAhmadEzani <wanahmaw@flip2.engr.oregonstate.edu>
2019-01-25 23:30:33 -0800

    Moved implementation for Council Room into a function outside of switch statement

--- projects/wanahmaw/dominion/dominion.c
+++ projects/wanahmaw/dominion/dominion.c
@@ -738,6 +738,32 @@ void run_Minion(struct gameState *state, int handPos, int currentPlayer, int cho
     }
 }
 
+void run_council_room(struct gameState *state, int currentPlayer, int handPos)
+{
+  int i;
+  
+  //+4 Cards
+  for (i = 0; i < 4; i++)
+    {
+      drawCard(currentPlayer, state);
+    }
+		    
+  //+1 Buy
+  state->numBuys++;
+		    
+  //Each other player draws a card
+  for (i = 0; i < state->numPlayers; i++)
+    {
+      if ( i != currentPlayer )
+	{
+	  drawCard(i, state);
+	}
+    }
+		    
+  //put played card in played card pile
+  discardCard(handPos, currentPlayer, state, 0);
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -759,31 +785,11 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     {
     case adventurer:
 	run_Adventurer(state);
-            return 0;
+        return 0;
 			
     case council_room:
-      //+4 Cards
-      for (i = 0; i < 4; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //+1 Buy
-      state->numBuys++;
-			
-      //Each other player draws a card
-      for (i = 0; i < state->numPlayers; i++)
-	{
-	  if ( i != currentPlayer )
-	    {
-	      drawCard(i, state);
-	    }
-	}
-			
-      //put played card in played card pile
-      discardCard(handPos, currentPlayer, state, 0);
-			
-      return 0;
+	run_council_room(state, currentPlayer, handPos);
+        return 0;
 			
     case feast:
       //gain card with cost up to 5
